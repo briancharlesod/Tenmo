e7279a737a6b71ed840dd2ef8b10354a1f3cd2fa
Michael Bissonnette <mbissonn@gmail.com>
2022-07-11 09:22:01 -0400

    added authorization for transfer controller

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -4,17 +4,19 @@ import com.techelevator.tenmo.dao.TransferDao;
 import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.model.Transfer;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+
+@PreAuthorize("isAuthenticated()")
 @RestController
 public class TransferController {
 
     private TransferDao transferDao;
     private UserDao userDao;
-
 
     public TransferController(TransferDao transferDao, UserDao userDao) {
         this.transferDao = transferDao;
@@ -28,7 +30,7 @@ public class TransferController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping( path = "/transfers", method = RequestMethod.POST)
-    public Transfer addTransfer(@RequestBody Transfer transfer) {
+    public Transfer addTransfer(@Valid @RequestBody Transfer transfer) {
          transferDao.createSend(transfer);
          return transfer;
     }
